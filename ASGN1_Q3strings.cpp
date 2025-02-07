1f4718c497a8265536d991a7e74496a65ae76690
Sia Gupta <guptasia.27@gmail.com>
2025-02-07 21:44:16 +0530

    ASGN1_Q3strings.cpp

--- ASGN1_Q3strings.cpp
+++ ASGN1_Q3strings.cpp
@@ -5,28 +5,35 @@ using namespace std;
 int main()
 {
     //input a string by user
-    char x[25];
+    char x[25],i,j;
     cout<<"Enter the string"<<endl;
     cin>>x;
     cout<<"The string is "<<x<<endl;
     //palindrome or not
     char y[25];
-     strcpy(y,x);
+    int len=strlen(x);
+    for(i=0;i<len;i++)
+    {x[i]=tolower(x[i]);}
+    strcpy(y,x);
     strrev(x);
     if(strcmp(x,y)==0){
     cout<<"The string is palindrone"<<endl;
     }
     else{
     cout<<"The string is not palindrone"<<endl;}
+    
     //count frequency of all characters
-    int i=0,j,alpha[26];
+    int alpha[26];
     for(i=0;i<26;i++)
     {alpha[i]=0;}
-    for(i=0;i<strlen(x);i++)
+    for(i=0;i<len;i++)
     {
         if(x[i]>='a'&& x[i]<='z')
         {j=x[i]-'a';
-        alpha[j]+=1;}
+        alpha[j]++;}
+        else if(x[i]>='A'&& x[i]<='Z')
+        {j=x[i]-'A';
+        alpha[j]++;}
     }
     for(i=0;i<26;i++)
     {if(alpha[i]>0)
